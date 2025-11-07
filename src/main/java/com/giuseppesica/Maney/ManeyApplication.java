e54974acf0b1bc7d568252ffcabd7b13772e04f5
Giuseppe Sica <giuseppesica03@gmail.com>
2025-11-07 12:49:58 +0100

    Initial commit

---
+++ src/main/java/com/giuseppesica/Maney/ManeyApplication.java
@@ -0,0 +1,13 @@
+package com.giuseppesica.Maney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManeyApplication.class, args);
+	}
+
+}
